aa03dd13cd6b8bf0db81f1f24140dcdad5276b7e
rajputana70 <nagesh@trainingbasket.co>
2026-09-10 13:14:37 +0530

    Fix typo in examStarted condition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen">
-      {!lests-examStarted ? (
+      {!examStarted ? (
         <WelcomeScreen onStartExam={handleStartExam} />
       ) : (
         <ExamContainer />
